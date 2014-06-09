91057b75c21b304a2c169852b322e20f9a4f6820
Chih-Yung Liang <stu89424@gmail.com>
2014-06-09 10:42:58 +0800

    argument fetch

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
 #define FIFO 0
 #define LRU 1
@@ -8,16 +9,13 @@ char *trace_path = NULL;
 int cache_size = -1, block_size = -1, associativity = -1, replacement = -1;
 
 void print_usage(const char *prog_name) {
-	printf(
-		"Usage: %s -t trace_file -s cache_size -l block_size -a associativity -r replacement\n \
-		arguments:\n \
-			trace_file: The path to the input file which contains those memory address to access.\n \
-			cache_size: The size of the cache. 1~1024 (KByte).\n \
-			block_size: The size of a cache block. 4~256 (Byte).\n \
-			associativity: The number of the way.\n \
-			replacement: 0 for FIFO and 1 for LRU.\n",
-		prog_name
-	);
+	cout <<	"Usage: " << prog_name << " -t trace_file -s cache_size -l block_size -a associativity -r replacement\n"
+	     << "arguments:\n"
+	     << "\ttrace_file: The path to the input file which contains those memory address to access.\n"
+	     << "\tcache_size: The size of the cache. 1~1024 (KByte).\n"
+	     << "\tblock_size: The size of a cache block. 4~256 (Byte).\n"
+	     << "associativity: The number of the way.\n"
+	     << "replacement: 0 for FIFO and 1 for LRU." << endl;
 
 	exit(2);
 }
@@ -37,33 +35,33 @@ void arg_fetch(int argc, char *argv[]) {
 		case 's':
 			cache_size = atoi(argv[1]);
 			if(cache_size < 1 || cache_size > 1024) {
-				puts("Invalid cache size.");
+				cout << "Invalid cache size.\n";
 				print_usage(prog_name);
 			}
 			break;
 		case 'l':
 			block_size = atoi(argv[1]);
 			if(block_size < 4 || block_size > 256) {
-				puts("Invalid block size.");
+				cout << "Invalid block size.\n";
 				print_usage(prog_name);
 			}
 			break;
 		case 'a':
 			associativity = atoi(argv[1]);
 			if(associativity != 0 || associativity != 1) {
-				puts("Invalid associativity value.");
+				cout << "Invalid associativity value.\n";
 				print_usage(prog_name);
 			}
 			break;
 		case 'r':
 			replacement = atoi(argv[1]);
 			if(replacement != FIFO || replacement != LRU) {
-				puts("Invalid replacement value.");
+				cout << "Invalid replacement value.\n";
 				print_usage(prog_name);
 			}
 			break;
 		default:
-			printf("Unknown option %s.\n", argv[0]);
+			cout << "Unknown option" << argv[0] << ".\n";
 			print_usage(prog_name);
 		}
 
@@ -71,7 +69,7 @@ void arg_fetch(int argc, char *argv[]) {
 		argv += 2;
 	}
 	if(trace_path == NULL || cache_size == -1 || block_size == -1 || associativity == -1 || replacement == -1) {
-		puts("Miss arguments.");
+		cout << "Miss arguments.\n";
 		print_usage(prog_name);
 	}
 }
