b2b7513c1735e4fd571491104459cca5b129ba29
Chih-Yung Liang <stu89424@gmail.com>
2014-06-09 16:18:50 +0800

    Finish the requirement.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <list>
 using namespace std;
@@ -6,8 +7,9 @@ using namespace std;
 #define FIFO 0
 #define LRU 1
 
-char *trace_path = NULL;
+char *trace_path;
 int cache_size = -1, block_size = -1, associativity = -1, replacement = -1;
+ifstream trace_file;
 
 void arg_fetch(int argc, char *argv[]);
 
@@ -19,7 +21,7 @@ class Cache {
 public:
 	Cache();
 	~Cache();
-	void getData(unsigned addr);
+	bool getData(unsigned addr);
 
 private:
 	Set *sets;
@@ -29,7 +31,7 @@ private:
 class Set {
 public:
 	Set();
-	void getData(unsigned tag);
+	bool getData(unsigned tag);
 
 private:
 	list<Block> blocks;
@@ -45,23 +47,45 @@ private:
 //	char *data; 			// Needn't to store the data in this assignment.
 	bool valid;
 	unsigned tag;
-}
+};
 
 
 int main(int argc, char *argv[]) {
 	arg_fetch(argc, argv);
+	cout << "Trace file: " << trace_path << '\n'
+	     << "Cache size: " << cache_size << '\n'
+	     << "Line size: " << block_size << '\n'
+	     << "Associativity: " << associativity << '\n'
+	     << "Replace: " << (replacement == LRU ? "LRU" : "FIFO") << '\n'
+	     << "Number of blocks= " << cache_size / block_size << '\n'
+	     << "Number of sets= " << cache_size / block_size / associativity << '\n';
+
+	Cache cache;
+	int hitCount = 0, missCount = 0;
+
+	trace_file.setf(ios::hex, ios::basefield);
+	unsigned addr;
+	while(trace_file >> addr) {
+		if(cache.getData(addr))
+			hitCount++;
+		else
+			missCount++;
+	}
+
+	cout << "Cache hits= " << hitCount << '\n'
+	     << "Cache miss= " << missCount << endl;
 
 	return 0;
 }
 
 void print_usage(const char *prog_name) {
-	cout <<	"Usage: " << prog_name << " -t trace_file -s cache_size -l block_size -a associativity -r replacement\n"
+	cerr <<	"Usage: " << prog_name << " -t trace_file -s cache_size -l block_size -a associativity -r replacement\n"
 	     << "arguments:\n"
 	     << "\ttrace_file: The path to the input file which contains those memory address to access.\n"
 	     << "\tcache_size: The size of the cache. 1~1024 (KByte).\n"
 	     << "\tblock_size: The size of a cache block. 4~256 (Byte).\n"
-	     << "associativity: The number of the way.\n"
-	     << "replacement: 0 for FIFO and 1 for LRU." << endl;
+	     << "\tassociativity: The number of the way.\n"
+	     << "\treplacement: 0 for FIFO and 1 for LRU." << endl;
 
 	exit(2);
 }
@@ -77,45 +101,51 @@ void arg_fetch(int argc, char *argv[]) {
 		switch(argv[0][1]) {
 		case 't':
 			trace_path = argv[1];
+			trace_file.open(trace_path);
+			if(!trace_file) {
+				cerr << "Cannot open file '" << trace_path << "'.\n";
+				print_usage(prog_name);
+			}
 			break;
 		case 's':
 			cache_size = atoi(argv[1]);
 			if(cache_size < 1 || cache_size > 1024) {
-				cout << "Invalid cache size.\n";
+				cerr << "Invalid cache size.\n";
 				print_usage(prog_name);
 			}
+			cache_size <<= 10;
 			break;
 		case 'l':
 			block_size = atoi(argv[1]);
 			if(block_size < 4 || block_size > 256) {
-				cout << "Invalid block size.\n";
+				cerr << "Invalid block size.\n";
 				print_usage(prog_name);
 			}
 			break;
 		case 'a':
 			associativity = atoi(argv[1]);
-			if(associativity != 0 || associativity != 1) {
-				cout << "Invalid associativity value.\n";
+			if(associativity > cache_size / block_size) {
+				cerr << "Invalid associativity value.\n";
 				print_usage(prog_name);
 			}
 			break;
 		case 'r':
 			replacement = atoi(argv[1]);
-			if(replacement != FIFO || replacement != LRU) {
-				cout << "Invalid replacement value.\n";
+			if(replacement != FIFO && replacement != LRU) {
+				cerr << "Invalid replacement value.\n";
 				print_usage(prog_name);
 			}
 			break;
 		default:
-			cout << "Unknown option" << argv[0] << ".\n";
+			cerr << "Unknown option" << argv[0] << ".\n";
 			print_usage(prog_name);
 		}
 
 		argc -= 2;
 		argv += 2;
 	}
-	if(trace_path == NULL || cache_size == -1 || block_size == -1 || associativity == -1 || replacement == -1) {
-		cout << "Miss arguments.\n";
+	if(!trace_file.is_open() || cache_size == -1 || block_size == -1 || associativity == -1 || replacement == -1) {
+		cerr << "Miss arguments.\n";
 		print_usage(prog_name);
 	}
 }
@@ -141,7 +171,7 @@ Set::Set() {
 	blocks.resize(associativity);
 }
 
-void Set::getData(unsigned tag) {
+bool Set::getData(unsigned tag) {
 	for(list<Block>::iterator it = blocks.begin(); it != blocks.end(); ++it) {
 		if(it->getData(tag)) {
 			if(replacement == LRU) {
@@ -150,7 +180,7 @@ void Set::getData(unsigned tag) {
 				blocks.push_back(temp);
 			}
 
-			return;
+			return true;
 		}
 	}
 
@@ -158,6 +188,7 @@ void Set::getData(unsigned tag) {
 	blocks.pop_front();
 	temp.replace(tag);
 	blocks.push_back(temp);
+	return false;
 }
 
 Cache::Cache() {
@@ -169,10 +200,10 @@ Cache::~Cache() {
 	delete[] sets;
 }
 
-Cache::getData(unsigned addr) {
+bool Cache::getData(unsigned addr) {
 	unsigned block_addr = addr / block_size;
 	unsigned index = block_addr % setCount;
 	unsigned tag = block_addr / setCount;
 
-	sets[index].getData(tag);
+	return sets[index].getData(tag);
 }
