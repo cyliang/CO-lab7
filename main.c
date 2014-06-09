acbe405fd5c13e5910d47c43fa5060679d7d3f5e
Chih-Yung Liang <stu89424@gmail.com>
2014-06-09 10:37:20 +0800

    Complete argument fetching

--- main.c
+++ main.c
@@ -7,10 +7,28 @@
 char *trace_path = NULL;
 int cache_size = -1, block_size = -1, associativity = -1, replacement = -1;
 
-int main(int argc, char *argv[]) {
+void print_usage(const char *prog_name) {
+	printf(
+		"Usage: %s -t trace_file -s cache_size -l block_size -a associativity -r replacement\n \
+		arguments:\n \
+			trace_file: The path to the input file which contains those memory address to access.\n \
+			cache_size: The size of the cache. 1~1024 (KByte).\n \
+			block_size: The size of a cache block. 4~256 (Byte).\n \
+			associativity: The number of the way.\n \
+			replacement: 0 for FIFO and 1 for LRU.\n",
+		prog_name
+	);
+
+	exit(2);
+}
+
+void arg_fetch(int argc, char *argv[]) {
+	const char *prog_name = argv[0];
+	++argv, --argc;
+
 	while(argc > 0) {
 		if(argv[0][0] != '-') {
-			exit(2);
+			print_usage(prog_name);
 		}
 		switch(argv[0][1]) {
 		case 't':
@@ -18,25 +36,35 @@ int main(int argc, char *argv[]) {
 			break;
 		case 's':
 			cache_size = atoi(argv[1]);
+			if(cache_size < 1 || cache_size > 1024) {
+				puts("Invalid cache size.");
+				print_usage(prog_name);
+			}
 			break;
 		case 'l':
 			block_size = atoi(argv[1]);
+			if(block_size < 4 || block_size > 256) {
+				puts("Invalid block size.");
+				print_usage(prog_name);
+			}
 			break;
 		case 'a':
 			associativity = atoi(argv[1]);
 			if(associativity != 0 || associativity != 1) {
-				exit(2);
+				puts("Invalid associativity value.");
+				print_usage(prog_name);
 			}
 			break;
 		case 'r':
 			replacement = atoi(argv[1]);
 			if(replacement != FIFO || replacement != LRU) {
-				exit(2);
+				puts("Invalid replacement value.");
+				print_usage(prog_name);
 			}
 			break;
 		default:
 			printf("Unknown option %s.\n", argv[0]);
-			exit(2);
+			print_usage(prog_name);
 		}
 
 		argc -= 2;
@@ -44,6 +72,12 @@ int main(int argc, char *argv[]) {
 	}
 	if(trace_path == NULL || cache_size == -1 || block_size == -1 || associativity == -1 || replacement == -1) {
 		puts("Miss arguments.");
-		exit(2);
+		print_usage(prog_name);
 	}
+}
+
+int main(int argc, char *argv[]) {
+	arg_fetch(argc, argv);
+
+	return 0;
 }
